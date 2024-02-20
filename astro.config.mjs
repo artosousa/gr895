d48ef03363711329d21968f8a3a84dd1cc2f189a
artosousa <artosousa@gmail.com>
2024-02-20 03:34:49 -0500

    GTA manager code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import partytown from '@astrojs/partytown'
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://34to70.com/',
+  integrations: [
+    tailwind(),
+    partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		}),
+  ],
   output: "server",
   adapter: netlify()
 });
